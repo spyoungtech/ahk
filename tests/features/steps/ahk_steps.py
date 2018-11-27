bc953e9ffce3c5c3b095882652be0ed833160a6f
Spencer Phillip Young <spencer.young@spyoung.com>
2018-11-27 00:40:04 -0800

    basic test and ci setup (#1)
    * use python37
    
    * add scripts, tests
    
    * use wheel
    
    * use build script
    
    * move coveralls to runtests

---
+++ tests/features/steps/ahk_steps.py
@@ -0,0 +1,32 @@
+import pyautogui
+from behave.matchers import RegexMatcher
+from ahk import AHK
+from behave_classy import step_impl_base
+
+Base = step_impl_base()
+
+
+class AHKSteps(AHK, Base):
+    @Base.given(u'the mouse position is ({xpos:d}, {ypos:d})')
+    def given_mouse_move(self, xpos, ypos):
+        self.mouse_move(x=xpos, y=ypos)
+
+    @Base.when(u'I move the mouse (UP|DOWN|LEFT|RIGHT) (\d+)px', matcher=RegexMatcher)
+    def move_direction(self, direction, px):
+        px = int(px)
+        if direction in ('UP', 'DOWN'):
+            axis = 'y'
+        else:
+            axis = 'x'
+        if direction in ('LEFT', 'UP'):
+            px = px * -1
+        kwargs = {axis: px, 'relative': True}
+        self.mouse_move(**kwargs)
+
+    @Base.then(u'I expect the mouse position to be ({xpos:d}, {ypos:d})')
+    def check_position(self, xpos, ypos):
+        x, y = self.mouse_position
+        assert x == xpos
+        assert y == ypos
+
+AHKSteps().register()
