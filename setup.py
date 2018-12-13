f48884e0d26cf34cbc75d00afa85453bd99779dc
Spencer Young <spencer.young@spyoung.com>
2018-12-12 19:33:01 -0800

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ extras = {'test': test_requirements}
 
 setup(
     name='ahk',
-    version='0.2.0',
+    version='0.2.1',
     url='https://github.com/spyoungtech/ahk',
     description='A Python wrapper for AHK',
     author_email='spencer.young@spyoung.com',
