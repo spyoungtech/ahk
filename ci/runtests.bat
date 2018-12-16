0b47f7d24e9b577e6b6e69b1d096e237da974ef5
Spencer Young <spencer.young@spyoung.com>
2018-12-15 17:41:02 -0800

    remove old runtests

--- ci/runtests.bat
+++
@@ -1,4 +0,0 @@
-call venv\Scripts\activate.bat
-coverage run -m behave tests\features --format=progress2 --junit
-coverage run -a -m pytest tests\unittests --junitxml=reports\pytestresults.xml
-call deactivate
