2d8e8ab95f84512bdc88007730bea048bc09f876
Spencer Young <spencer.young@spyoung.com>
2019-01-29 23:20:51 -0800

    better logging practice

--- ahk/utils.py
+++ ahk/utils.py
@@ -1,5 +1,5 @@
-import os
 import logging
+
 
 def make_logger(name):
     logger = logging.getLogger(name)
@@ -8,8 +8,4 @@ def make_logger(name):
             '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
     handler.setFormatter(formatter)
     logger.addHandler(handler)
-    if os.environ.get('AHK_DEBUG'):
-        logger.setLevel(logging.DEBUG)
-    else:
-        logger.setLevel(logging.ERROR)
     return logger
