9bf1d85fb4ef0c1d2558b95fba675d725df2d479
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2019-07-25 21:33:16 -0700

    depreciated Commicator, and removed some extra code

--- ahk/communication.py
+++ ahk/communication.py
@@ -9,52 +9,26 @@ from ahk.utils import make_logger
 
 logger = make_logger(__name__)
 
+
 class Communicator:
+    """
+The following is a personal note
+
+from ahk import AHK
+ahk = AHK()
+from ahk import Bindable_Hotkey
+hotkey = Bindable_Hotkey(ahk, 'j', lambda: print("Lambdaaaaaaa"))
+    """
+
     def __init__(self):
+        logger.debug("Initing Communicator")
         self.EventListener = EventListener()
+        self.module_path = os.path
 
+    def all_stop(self):
+        self.EventListener.stop()
+        # Add the other communicator when made
 
-
-class EventListener(Abstract_Communicator):
-
-    code_dict={}    
-
-    def __init__(self):
-        super().__init__(pathlib.Path(
-            os.path.abspath(os.path.dirname(__file__))).parents[0]/"tmp")
-
-    def on_event(self):
-        changed_files = self.get_changed_file()
-        for i in changed_files:
-            self._call_keycode(os.path.basename(i))
-
-    def _call_keycode(self, code):
-
-        logger.debug(code)
-        try:
-            functions = self.code_dict[code]
-            for i in functions:
-                logger.debug(i)
-                i()
-        except KeyError:
-            logger.info("not my keycode!")
-
-    def add(self, keycode, function_to_bind):
-        try:
-            existing = self.code_dict[keycode]
-            existing.append(function_to_bind)
-            self.code_dict[keycode] = existing
-        except KeyError:
-            self.code_dict[keycode] = [function_to_bind]
-
-    def remove(self, keycode, function_to_bind):
-        try:
-            existing = self.code_dict[keycode]
-        except KeyError:
-            return False
-        for i in range(0, len(existing)-1):
-            if existing[i] == function_to_bind:
-                del existing[i]
 
 class Abstract_Communicator(metaclass=abc.ABCMeta):
 
@@ -77,6 +51,7 @@ class Abstract_Communicator(metaclass=abc.ABCMeta):
         
         self.stop_thread = False
         self.thread = threading.Thread(target=self.event_loop)
+        self.thread.daemon = True
         self.thread.start()
 
     def stop_loop(self):
@@ -86,10 +61,9 @@ class Abstract_Communicator(metaclass=abc.ABCMeta):
         self.stop_thread = True
         for i in os.listdir(str(self.path)):
             os.remove(i)
-        logging.debug("deleting abstract_communication") 
+        logger.debug("deleting abstract_communication") 
 
     def get_changed_file(self)->set:
-        self.path
         # Some fancy pants dictionary comprehension that uses the path to cycle through
         # all of the files in the path, make the file path the key, and set the value
         # to the last time it was modified
@@ -121,8 +95,8 @@ class Abstract_Communicator(metaclass=abc.ABCMeta):
 
         try:
             while self.stop_thread == False:
-                logging.debug("Looping")
-                logging.debug(self.stop_thread)
+                logger.debug("Looping")
+                logger.debug(self.stop_thread)
                 result = win32event.WaitForSingleObject (change_handle, 500)
 
                 #
@@ -133,7 +107,53 @@ class Abstract_Communicator(metaclass=abc.ABCMeta):
                 if result == win32con.WAIT_OBJECT_0:
                     self.on_event()
                 win32file.FindNextChangeNotification (change_handle)
-
+        except KeyboardInterrupt:
+            self.stop_thread = True
         finally:
             win32file.FindCloseChangeNotification (change_handle)
-            logging.debug("stopping notification loop")
+            logger.debug("stopping notification loop")
+
+class EventListener(Abstract_Communicator):
+
+    code_dict={}    
+
+    def __init__(self):
+        super().__init__(pathlib.Path(
+            os.path.abspath(os.path.dirname(__file__))).parents[0]/"tmp")
+
+    def on_event(self):
+        changed_files = self.get_changed_file()
+        for i in changed_files:
+            self._call_keycode(os.path.basename(i))
+
+    def stop(self):
+        logger.debug("Stopping thread from EventListener")
+        self.stop_thread = True
+
+    def _call_keycode(self, code):
+
+        logger.debug(code)
+        try:
+            functions = self.code_dict[code]
+            for i in functions:
+                logger.debug(i)
+                i()
+        except KeyError:
+            logger.info("not my keycode!")
+
+    def add(self, keycode, function_to_bind):
+        try:
+            existing = self.code_dict[keycode]
+            existing.append(function_to_bind)
+            self.code_dict[keycode] = existing
+        except KeyError:
+            self.code_dict[keycode] = [function_to_bind]
+
+    def remove(self, keycode, function_to_bind):
+        try:
+            existing = self.code_dict[keycode]
+        except KeyError:
+            return False
+        for i in range(0, len(existing)-1):
+            if existing[i] == function_to_bind:
+                del existing[i]
