8156d0903bac1937a830cde90b0ed9d06fc5547f
Yunus Emre <49655146+yedhrab@users.noreply.github.com>
2020-02-25 12:22:31 +0300

    ⚗️ More test methods added

--- tests/unittests/test_window.py
+++ tests/unittests/test_window.py
@@ -14,6 +14,24 @@ class TestWindow(TestCase):
         self.win = self.ahk.win_get(title='Untitled - Notepad')
         self.assertIsNotNone(self.win)
 
+    def test_transparent(self):
+        self.assertEqual(self.win.transparent, 255)
+
+        self.win.transparent = 220
+        self.assertEqual(self.win.transparent, 220)
+
+        self.win.transparent = 255
+        self.assertEqual(self.win.transparent, 255)
+
+    def test_pinned(self):
+        self.assertFalse(self.win.always_on_top)
+
+        self.win.always_on_top = True
+        self.assertTrue(self.win.always_on_top)
+
+        self.win.always_on_top = False
+        self.assertFalse(self.win.always_on_top)
+
     def test_close(self):
         self.win.close()
         self.assertFalse(self.win.exist)
@@ -30,6 +48,8 @@ class TestWindow(TestCase):
         self.assertFalse(self.win.exist)
 
     def test_max_min(self):
+        self.assertTrue(self.win.non_max_non_min)
+
         self.win.maximize()
         self.assertTrue(self.win.maximized)
 
@@ -39,5 +59,19 @@ class TestWindow(TestCase):
         self.win.restore()
         self.assertTrue(self.win.maximized)
 
+    def test_names(self):
+        self.assertEqual(self.win.class_name, b'Notepad')
+        self.assertEqual(self.win.title, b'Untitled - Notepad')
+        self.assertEqual(self.win.text, b'')
+
     def tearDown(self):
         self.p.terminate()
+
+
+if __name__ == "__main__":
+    ahk = AHK()
+    p = subprocess.Popen('notepad')
+    time.sleep(1)
+    win = ahk.win_get(title='Untitled - Notepad')
+    print(win.transparent)
+    win.transparent = 255
