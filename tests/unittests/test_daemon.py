3657675f575fe72177ed74b39b59f46c88c883a1
Spencer Young <spencer.young@spyoung.com>
2021-01-10 13:32:52 -0800

    add winget tests

--- tests/unittests/test_daemon.py
+++ tests/unittests/test_daemon.py
@@ -9,7 +9,7 @@ from itertools import product
 project_root = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../..'))
 sys.path.insert(0, project_root)
 from ahk.daemon import AHKDaemon
-from ahk.window import AsyncWindow
+from ahk.window import AsyncWindow, WindowNotFoundError
 from PIL import Image
 
 class TestMouseDaemon(IsolatedAsyncioTestCase):
@@ -154,3 +154,43 @@ class TestScreenDaemon(IsolatedAsyncioTestCase):
         result = await self.ahk.pixel_get_color(x, y)
         self.assertIsNotNone(result)
         self.assertEqual(int(result, 16), 0xFF0000)
+
+
+class TestWinGetDaemon(IsolatedAsyncioTestCase):
+    async def asyncSetUp(self):
+        self.ahk = AHKDaemon()
+        await self.ahk.start()
+        self.p = subprocess.Popen('notepad')
+        time.sleep(1)
+        self.win = await self.ahk.win_get(title='Untitled - Notepad')
+        self.assertIsNotNone(self.win)
+
+    async def asyncTearDown(self):
+        self.p.terminate()
+        await asyncio.sleep(0.5)
+        self.ahk.stop()
+
+
+    async def test_get_calculator(self):
+        assert await self.win.position
+
+
+    async def test_win_close(self):
+        await self.win.close()
+        try:
+            win = await self.ahk.win_get(title='Untitled - Notepad')
+            await win.position
+        except WindowNotFoundError as e:
+            pass
+        else:
+            raise AssertionError("Expected WindowNotFoundError")
+
+    async def test_find_window_func(self):
+        async def func(win):
+            return b'Untitled' in await win.title
+        assert self.win == await self.ahk.find_window(func=func)
+
+    async def test_getattr_window_subcommand(self):
+        assert isinstance(await self.win.pid, str)
+
+
