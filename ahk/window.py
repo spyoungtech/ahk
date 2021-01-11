676383b149e9ce3e3cd3df3ebd22d3b90f5158ce
Spencer Young <spencer.young@spyoung.com>
2021-01-10 17:01:06 -0800

    replace getattr

--- ahk/window.py
+++ ahk/window.py
@@ -836,11 +836,11 @@ class AsyncWindow(Window):
         ahk_id = await engine.a_run_script(script)
         return cls(engine=engine, ahk_id=ahk_id, **kwargs)
 
-    # def __getattr__(self, item):
-    #     if item in self._get_subcommands:
-    #         raise AttributeError(f"Unaccessable Attribute. Use get({item}) instead")
-    #     raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{item}'")
-    #
+    def __getattr__(self, item):
+        if item in self._get_subcommands:
+            raise AttributeError(f"Unaccessable Attribute. Use get({item}) instead")
+        raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{item}'")
+
     async def get_pos(self, info=None):
         script = self._get_pos(info)
         resp = await self.engine.a_run_script(script)
