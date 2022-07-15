83b8c689e27af22528736a4b46050372f608bdc7
Spencer Phillip Young <spencer.young@spyoung.com>
2022-07-15 12:46:20 -0700

    add sync tests

---
+++ tests/_sync/test_screen.py
@@ -0,0 +1,44 @@
+import asyncio
+import time
+from itertools import product
+from unittest import IsolatedAsyncioTestCase
+from unittest import TestCase
+
+from PIL import Image
+
+from ahk import AHK
+
+
+class TestScreen(IsolatedAsyncioTestCase):
+    def asyncSetUp(self) -> None:
+        self.ahk = AHK()
+        self.before_windows = self.ahk.list_windows()
+        im = Image.new('RGB', (20, 20))
+        for coord in product(range(20), range(20)):
+            im.putpixel(coord, (255, 0, 0))
+        self.im = im
+        im.show()
+        time.sleep(2)
+
+    def asyncTearDown(self):
+        for win in self.ahk.list_windows():
+            if win not in self.before_windows:
+                win.close()
+                break
+        self.ahk._transport._proc.kill()
+
+    #
+    # async def test_pixel_search(self):
+    #     result = await self.ahk.pixel_search(0xFF0000)
+    #     self.assertIsNotNone(result)
+
+    def test_image_search(self):
+        self.im.save('testimage.png')
+        position = self.ahk.image_search('testimage.png')
+        assert isinstance(position, tuple)
+
+    # async def test_pixel_get_color(self):
+    #     x, y = await self.ahk.pixel_search(0xFF0000)
+    #     result = await self.ahk.pixel_get_color(x, y)
+    #     self.assertIsNotNone(result)
+    #     self.assertEqual(int(result, 16), 0xFF0000)
---
+++ tests/_sync/test_window.py
@@ -0,0 +1,26 @@
+import asyncio
+import os
+import subprocess
+import sys
+import time
+from unittest import IsolatedAsyncioTestCase
+
+from ahk import AHK
+from ahk import Window
+
+
+# class TestWindowAsync(IsolatedAsyncioTestCase):
+#     win: AsyncWindow
+#
+#     async def asyncSetUp(self) -> None:
+#         self.ahk = AsyncAHK()
+#         self.p = subprocess.Popen('notepad')
+#         time.sleep(1)
+#         self.win = await self.ahk.win_get(title='Untitled - Notepad')
+#         self.assertIsNotNone(self.win)
+#
+#     async def test_close(self):
+#         await self.win.close()
+#         await asyncio.sleep(0.2)
+#         self.assertFalse(await self.win.exists())
+#         self.assertFalse(await self.win.exist)
