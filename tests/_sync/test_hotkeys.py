0eb45e5dac5fb8c97f612b23bc3050ba568ebe30
Spencer Phillip Young <spencer.young@spyoung.com>
2022-07-20 21:35:29 -0700

    fix pre-commit fail

--- tests/_sync/test_hotkeys.py
+++ tests/_sync/test_hotkeys.py
@@ -1,35 +1,33 @@
 import asyncio
 import time
-from unittest import IsolatedAsyncioTestCase  # unasync: remove
 from unittest import mock
 from unittest import TestCase
 
-from ahk import AsyncAHK
-from ahk import AsyncWindow
+from ahk import AHK
+from ahk import Window
 
-async_sleep = asyncio.sleep  # unasync: remove
 
 sleep = time.sleep
 
 
-class TestMouseAsync(IsolatedAsyncioTestCase):
-    win: AsyncWindow
+class TestMouseAsync(TestCase):
+    win: Window
 
-    async def asyncSetUp(self) -> None:
-        self.ahk = AsyncAHK()
+    def setUp(self) -> None:
+        self.ahk = AHK()
 
-    async def asyncTearDown(self) -> None:
+    def tearDown(self) -> None:
         self.ahk.stop_hotkeys()
         self.ahk._transport._proc.kill()
 
-    async def test_hotkey(self):
+    def test_hotkey(self):
         with mock.MagicMock(return_value=None) as m:
             self.ahk.add_hotkey('a', callback=m)
             self.ahk.start_hotkeys()
-            await self.ahk.key_down('a')
+            self.ahk.key_down('a')
             m.assert_called()
 
-    async def test_hotkey_ex_handler(self):
+    def test_hotkey_ex_handler(self):
         def side_effect():
             raise Exception('oh no')
 
@@ -37,5 +35,5 @@ class TestMouseAsync(IsolatedAsyncioTestCase):
             mock_cb.side_effect = side_effect
             self.ahk.add_hotkey('a', callback=mock_cb, ex_handler=mock_ex_handler)
             self.ahk.start_hotkeys()
-            await self.ahk.key_down('a')
+            self.ahk.key_down('a')
             mock_ex_handler.assert_called()
