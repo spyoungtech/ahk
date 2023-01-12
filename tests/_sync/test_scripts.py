c02949dac0eb17d935c991032c5c365d594a5422
Spencer Phillip Young <spencer.young@spyoung.com>
2023-01-11 17:28:36 -0800

    add test script

--- tests/_sync/test_scripts.py
+++ tests/_sync/test_scripts.py
@@ -2,26 +2,26 @@ import pathlib
 import time
 import unittest.mock
 
-from ahk import AsyncAHK
-from ahk import AsyncWindow
+from ahk import AHK
+from ahk import Window
 
 
-class TestScripts(unittest.IsolatedAsyncioTestCase):
-    win: AsyncWindow
+class TestScripts(unittest.TestCase):
+    win: Window
 
-    async def asyncSetUp(self) -> None:
-        self.ahk = AsyncAHK()
+    def setUp(self) -> None:
+        self.ahk = AHK()
 
-    async def asyncTearDown(self) -> None:
+    def tearDown(self) -> None:
         try:
             self.ahk._transport._proc.kill()
         except:
             pass
         time.sleep(0.2)
 
-    async def test_script_missing_makes_tempfile(self):
+    def test_script_missing_makes_tempfile(self):
         with mock.patch('os.path.exists', new=unittest.mock.Mock(return_value=False)):
-            pos = await self.ahk.get_mouse_position()
+            pos = self.ahk.get_mouse_position()
             path = pathlib.Path(self.ahk._transport._proc.runargs[-1])
             filename = path.name
             assert filename.startswith('python-ahk-')
